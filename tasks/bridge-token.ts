360a479c96f7e46989afa4dfe465c049b3c1a5a3
DruuuCLT <121195249+DruuuCLT@users.noreply.github.com>
2024-01-31 23:22:11 +0100

    Harmony support for token briding task

--- tasks/bridge-token.ts
+++ tasks/bridge-token.ts
@@ -1,27 +1,33 @@
 import { task } from "hardhat/config";
 
 task("bridge-token", "")
-    .addParam("dest", "Destination chain id")
+	.addParam("dest", "Destination chain id")
 	.addParam("amount", "Amount of tokens in ETH")
 	.addOptionalParam("wallet", "Custom wallet")
 	.addOptionalParam("signer", "Custom signer (private key)")
+	.addOptionalParam("gas", "Custom gas")
 	.addOptionalParam("provider", "Custom provider RPC url")
-	.setAction(async (args, hre:any) => {
+	.setAction(async (args, hre: any) => {
 		const ethers = hre.ethers;
 		const network = hre.network.name;
 		const [deployer] = await ethers.getSigners();
-        
+
+		const GAS_LIMIT = 0x500000;
+
 		let signer = deployer;
 		let wallet = deployer.address;
 		if (args.signer) signer = new ethers.Wallet(args.signer, new ethers.providers.JsonRpcProvider(args.provider));
-		if (args.wallet) wallet = args.wallet; 
+		if (args.wallet) wallet = args.wallet;
 
 		const helloERC20 = await ethers.getContract("HelloERC20");
 
-		await(await helloERC20.connect(signer).approve(signer.address, ethers.parseEther(args.amount))).wait();
+		let overrides = {};
+		if (args.gas) overrides = { gasLimit: GAS_LIMIT };
 
-		await helloERC20.connect(signer).bridge.estimateGas(args.dest, wallet, ethers.parseEther(args.amount));
-		await(await helloERC20.connect(signer).bridge(args.dest, wallet, ethers.parseEther(args.amount))).wait();
+		// await (await helloERC20.connect(signer).approve(signer.address, ethers.parseEther(args.amount), overrides)).wait();
 
-		console.log('sent',args.amount,' of tokens to', wallet, 'on chain id', args.dest);
+		// await helloERC20.connect(signer).bridge.estimateGas(args.dest, wallet, ethers.parseEther(args.amount), overrides);
+		await (await helloERC20.connect(signer).bridge(args.dest, wallet, ethers.parseEther(args.amount), overrides)).wait();
+
+		console.log("sent", args.amount, " of tokens to", wallet, "on chain id", args.dest);
 	});
