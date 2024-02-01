3adcf1a60aa3349ecd43c14561be226b3676eaaa
DruuuCLT <121195249+DruuuCLT@users.noreply.github.com>
2024-02-01 23:34:48 +0100

    Update bridge-token.ts

--- tasks/bridge-token.ts
+++ tasks/bridge-token.ts
@@ -5,14 +5,11 @@ task("bridge-token", "")
 	.addParam("amount", "Amount of tokens in ETH")
 	.addOptionalParam("wallet", "Custom wallet")
 	.addOptionalParam("signer", "Custom signer (private key)")
-	.addOptionalParam("gas", "Custom gas")
 	.addOptionalParam("provider", "Custom provider RPC url")
 	.setAction(async (args, hre: any) => {
 		const ethers = hre.ethers;
 		const network = hre.network.name;
 		const [deployer] = await ethers.getSigners();
-
-		const GAS_LIMIT = 0x500000;
 
 		let signer = deployer;
 		let wallet = deployer.address;
@@ -21,13 +18,10 @@ task("bridge-token", "")
 
 		const helloERC20 = await ethers.getContract("HelloERC20");
 
-		let overrides = {};
-		if (args.gas) overrides = { gasLimit: GAS_LIMIT };
+		await (await helloERC20.connect(signer).approve(signer.address, ethers.parseEther(args.amount))).wait();
 
-		// await (await helloERC20.connect(signer).approve(signer.address, ethers.parseEther(args.amount), overrides)).wait();
-
-		// await helloERC20.connect(signer).bridge.estimateGas(args.dest, wallet, ethers.parseEther(args.amount), overrides);
-		await (await helloERC20.connect(signer).bridge(args.dest, wallet, ethers.parseEther(args.amount), overrides)).wait();
+		await helloERC20.connect(signer).bridge.estimateGas(args.dest, wallet, ethers.parseEther(args.amount));
+		await (await helloERC20.connect(signer).bridge(args.dest, wallet, ethers.parseEther(args.amount))).wait();
 
 		console.log("sent", args.amount, " of tokens to", wallet, "on chain id", args.dest);
 	});
